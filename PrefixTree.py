647e125591d2a314f3573822115a1c931a0842d0
Kane Maxcy <kanemaxcy@gmail.com>
2018-02-23 18:27:20 -0600

    Completed docstrings and added one comment.

--- PrefixTree.py
+++ PrefixTree.py
@@ -19,9 +19,8 @@ class Node():
         '''
         self.char = str(char) 
         
-        # Dictionary: key = str character, value = Node(character)
+        # Dictionary: children[a character] = Node object of that character
         self.children = {}
-
         self.is_root = False
         self.is_end = False
 
@@ -33,7 +32,7 @@ class Node():
             idx:    index of the current character in the word (default 0)
 
         Returns:
-            VOID.
+            VOID
 
         '''
         if idx == len(word):
@@ -54,14 +53,20 @@ class Node():
         next_node.insert_word(word, idx + 1)
 
     def is_word(self, word, idx=0):
-        ''' 
+        '''Traverses Trie for each character in input word
+
+        Args:
+            word:   word to check/traverse through
+            idx:    inddex for each character in word
+
+        Returns:
+            Boolean: If word is in the Trie 
 
         '''
-
         if self.is_root and word[idx] in self.children:
             return self.children[word[idx]].is_word(word, idx + 1)
 
-        if idx == len(word) or word[idx] not in self.children: 
+        if idx == len(word) or word[idx] not in self.children: # passed last char
             return False
 
         elif idx == len(word) - 1 and self.children[word[idx]].is_end is True:
